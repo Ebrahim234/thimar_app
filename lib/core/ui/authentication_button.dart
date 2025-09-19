22c2bdc2bb2e4553342816183c52a75613cdc41a
Ebrahim234 <ebrahimelsayed2342004@gmail.com>
2025-09-19 22:40:12 +0300

    completing task1

---
+++ lib/core/ui/authentication_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GreenButton extends StatelessWidget {
+  const GreenButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
